a0b503d7f4fcbc2576fdb5607dffd98b365b4733
AngelHernandez410 <angelchumpi2001@gmail.com>
2022-11-14 22:44:32 -0400

    Se agrega el proyecto

---
+++ cursos.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cursos</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="shortcut icon" href="img/solologo.png" type="image/x-icon">
+</head>
+<body>
+<header>
+        <a href="index.php" class="logo">
+            <div class="logo">
+                <img src="img/Logo.png" alt="Logo de la compañía">
+            </div>
+            <nav>
+                <a href="empresas.html" class="nav-kink">Empresas</a>
+                <a href="cursos.php" class="nav-kink">Cursos</a>
+                <a href="" class="nav-kink">Contacto</a>
+                <a href="" class="nav-kink1">FORMULARIO</a> 
+            </nav>
+        </a>
+    </header>
+    <main class = "back-cur">
+        <h1 class="titulo"><b>Cursos</b></h1>
+        <div class="bodycard">
+            <div class="card">
+                <div class="face front">
+                    <img src="img/asisadmin.jpg" alt="">
+                    <h3>Asistente Administrativo</h3>
+                </div>
+                <div class="face back">
+                    <h3>Asistente Administrativo</h3>
+                    <p><b>Requisitos:</b> Cursado bachillerato y con inclinaciones a estudios superiores en Adminitracion</p>
+                    <p><b>Duración:</b> 1 año y 4 meses. (1.600 horas académicas teóricas-prácticas)</p>
+                    <p><b>Horario:</b> Lunes a Viernes de 3:00pm a 6:00pm (para grupos de mínimo 15 personas)</p>
+                    <div class="edad">
+                        <p><b>Mínima edad requerida: 16 hasta 19 años NO CUMPLIDOS</b></p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="face front">
+                    <img src="img/asiscont.jpg" alt="">
+                    <h3>Asistente Contable</h3>
+                </div>
+                <div class="face back">
+                    <h3>Asistente Contable</h3>
+                    <p><b>Requisitos:</b> Cursado bachillerato y con inclinaciones a estudios superiores en contaduría</p>
+                    <p><b>Duración:</b> 5 meses. (500 horas académicas teóricas-prácticas)</p>
+                    <p><b>Horario:</b> Lunes a Viernes de 10:00am a 1:00pm (para grupos de mínimo 15 personas)</p>
+                    <div class="edad">
+                        <p><b>Mínima edad requerida: 16 hasta 19 años NO CUMPLIDOS</b></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bodycard">
+            <div class="card">
+                <div class="face front">
+                    <img src="img/asisinfor.jpg" alt="">
+                    <h3>Asistente en Informática</h3>
+                </div>
+                <div class="face back">
+                    <h3>Asistente en Informática</h3>
+                    <p><b>Requisitos:</b> Cursado bachillerato y con inclinaciones a estudios superiores en ciencias de la informática</p>
+                    <p><b>Duración:</b> 8 meses. (800 horas académicas teóricas-prácticas)</p>
+                    <p><b>Horario:</b> Lunes a Viernes de 1:50pm a 4:20pm (para grupos de mínimo 25 personas)</p>
+                    <div class="edad">
+                        <p><b>Mínima edad requerida: 16 hasta 19 años NO CUMPLIDOS</b></p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="face front">
+                    <img src="img/intfinan.jpg" alt="">
+                    <h3>Integral Financiero</h3>
+                </div>
+                <div class="face back">
+                    <h3>Integral Financiero</h3>
+                    <p><b>Requisitos:</b> Cursado bachillerato y con inclinaciones a estudios financieros superiores</p>
+                    <p><b>Duración:</b> 1 año. (1.200 horas académicas teóricas-prácticas)</p>
+                    <p><b>Horario:</b> Lunes a Jueves de 7:00am a 1:00pm (para grupos de mínimo 20 personas)</p>
+                    <div class="edad">
+                        <p><b>Mínima edad requerida: 16 hasta 19 años NO CUMPLIDOS</b></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="contenedor">
+            <h2 class="subtitulo"><b>Requisitos</b></h2>
+            <div class="requisitos">
+                <div class="rectangulos">
+                    <h1>Inscripción</h1>
+                    <a class="btn btn-1" href="pdf/requisitos_inscripcion.pdf" target="_blank">Descargar PDF</a>
+                </div>
+                <div class="rectangulos">
+                    <h1>Ley del INCES</h1>
+                    <a class="btn btn-1" href="pdf/ley_del_inces.pdf" target="_blank">Descargar PDF</a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="contenedor footer-content">
+            <div class="empresa-info">
+                <h2 class="brand">Instituto Codado</h2>
+                <p>Desde la formación de oficios hasta la especialización <br>en nuestros diplomados</p>
+            </div>
+            <div class="social-media">
+                <a href="https://es-la.facebook.com/" class="social-media-icon">
+                    <i class='bx bxl-facebook'></i>
+                </a>
+                <a href="https://twitter.com/?lang=es" class="social-media-icon">
+                    <i class='bx bxl-twitter' ></i>
+                </a>
+                <a href="https://www.instagram.com/?hl=es" class="social-media-icon">
+                    <i class='bx bxl-instagram' ></i>
+                </a>
+            </div>
+        </div>
+        <div class="line"></div>
+        <div class="banner-b">
+            <p class="Copy">Copyright © by Hernández-Rojas</p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instituto Codado</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="shortcut icon" href="img/solologo.png" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <a href="index.php" class="logo">
+            <div class="logo">
+                <img src="img/Logo.png" alt="Logo de la compañía">
+            </div>
+            <nav>
+                <a href="empresas.html" class="nav-kink">Empresas</a>
+                <a href="cursos.php" class="nav-kink">Cursos</a>
+                <a href="" class="nav-kink">Contacto</a>
+                <a href="" class="nav-kink1">FORMULARIO</a> 
+            </nav>
+        </a>
+    </header>
+    <div class = "banner">
+        <div class="fondo-negro">
+            <div class="banner-c">
+                <img src="img/solologo.png" alt="" class="img-inicio">
+                <h1>Instituto Codado</h1>
+                <h2>Desde la formación de oficios hasta la especialización en nuestros diplomados</h2>
+            </div>
+        </div>
+    </div>
+    <main>
+        <section class="contenedor">
+            <h2 class="subtitulo"><b>Acerca de Nosotros</b></h2>
+            <div class="contenedor-aboutus">
+                <img src="img/aboutus.jpg" alt="" class="img-about">
+                <div class="info-aboutus">
+                    <p>Somos una empresa con 44 años de experiencia en el área de Capacitación y Consultoría. 
+                        Realizamos el máximo esfuerzo para garantizar el crecimiento profesional del recurso humano de nuestros clientes, 
+                        gracias a la oferta de los más avanzados e innovadores cursos y programas de capacitación.</p>
+                </div>
+            </div>
+        </section>
+        <section class="contenedor" style='background-color: rgb(242, 242, 242);'>
+            <h2 class="subtitulo"><b>Misión</b></h2>
+            <div class="contenedor-aboutus">
+                <div class="info-aboutus">
+                    <p>Permanecer en el tiempo como la solución de nuestros clientes en el desarrollo y fortalecimiento de su recurso 
+                        humano, a través de la alta calidad de nuestros programas; garantizando un sistema de trabajo que provea 
+                        conocimiento y bienestar profesional.</p>
+                </div>
+                <img src="img/mision.jpg" alt="" class="img-about">
+            </div>
+        </section>
+        <section class="contenedor">
+            <h2 class="subtitulo"><b>Visión</b></h2>
+            <div class="contenedor-aboutus">
+                <img src="img/vision.jpg" alt="" class="img-about">
+                <div class="info-aboutus">
+                    <p>Ser reconocidos internacionalmente como la Empresa Líder en consultoría y capacitación de personal, contribuyendo 
+                        al crecimiento y cambio organizacional de nuestros clientes.</p>
+                </div>
+            </div>
+        </section>
+        <section class="contenedor" style='background-color: rgb(242, 242, 242);'>
+            <h2 class="subtitulo"><b>Valor Agregado</b></h2>
+            <div class="contenedor-aboutus">
+                <div class="info-aboutus">
+                    <p>Ofrecemos soluciones para la formación, a nivel Nacional, de grandes grupos de personas de las empresas y/o trabajadores
+                         temporales que se desempeñen en proyectos especiales.</p>
+                </div>
+                <img src="img/estudio.jpg" alt="" class="img-about">
+            </div>
+        </section>
+        <div class="fondo-cursos">
+            <div class = fondo-negro2>
+                <div class="banner-c">
+                    <h1>Cursos</h1>
+                    <p>Los programas de adiestramiento de Instituto CODADO, C.A. poseen reconocimientos de nuestros clientes y se 
+                        encuentran registrados ante el INCES bajo el Nº 327.548 y ante el Ministerio del Poder Popular para la 
+                        Educación bajo el código Nº PR0161507.La escogencia de los cursos y la adaptación de contenidos programáticos 
+                        se realizará en función a las necesidades y requerimientos del Cliente.
+                    </p>
+                    <a class="btn btn-1" href="cursos.php">Ver cursos</a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="contenedor footer-content">
+            <div class="empresa-info">
+                <h2 class="brand">Instituto Codado</h2>
+                <p>Desde la formación de oficios hasta la especialización <br>en nuestros diplomados</p>
+            </div>
+            <div class="social-media">
+                <a href="https://es-la.facebook.com/" class="social-media-icon">
+                    <i class='bx bxl-facebook'></i>
+                </a>
+                <a href="https://twitter.com/?lang=es" class="social-media-icon">
+                    <i class='bx bxl-twitter' ></i>
+                </a>
+                <a href="https://www.instagram.com/?hl=es" class="social-media-icon">
+                    <i class='bx bxl-instagram' ></i>
+                </a>
+            </div>
+        </div>
+        <div class="line"></div>
+        <div class="banner-b">
+            <p class="Copy">Copyright © by Hernández-Rojas</p>
+        </div>
+    </footer>
+</body>
+</html>
